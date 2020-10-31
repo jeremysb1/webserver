6316d7ec11391572453e8837a14484d48a1285b5
Jeremy Hall <jeremyhall@jeremys-mbp.attlocal.net>
2020-10-31 13:54:38 -0400

    adding new files

--- server.py
+++ server.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, render_templates
 app = Flask(__name__)
 
 @app.route('/')
 def hello_word():
-	return 'yo'
+	return render_template('index.html')
+
+@app.route('/about.html')
+def about():
+	return render_template('about.html')
+
+@app.route('/blog')
+def blog():
+	return 'not hotdog'
+
+@app.route('/blog/2020/donkey')
+def blog2():
+	return 'you are a space donkey'
