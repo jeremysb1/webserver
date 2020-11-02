fed4e73952e9c0310ff610096a74e6bea1e0d2ef
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2020-11-02 16:13:29 -0500

    major updates

--- server.py
+++ server.py
@@ -1,23 +1,11 @@
-from flask import Flask, render_templates, url_for
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
 @app.route('/')
-def yo():
+def home():
 	return render_template('index.html')
 
-@app.route('/about.html')
-def about():
-	return render_template('about.html')
-
-@app.route('/components.html')
-def component():
-	return render_template('components.html')
-
-@app.route('/works.html')
-def works():
-	return render_template('works.html')
-
-@app.route('/contact.html')
-def contact():
-	return render_template('contact.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+	return render_template(page_name)
