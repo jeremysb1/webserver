e6e7d6ca8977ced8a9c3d4b9e873fed8c6c948bf
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2020-11-02 17:04:16 -0500

    new functionality

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 
@@ -9,3 +9,12 @@ def home():
 @app.route('/<string:page_name>')
 def html_page(page_name):
 	return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+    	data = request.form.to_dict()
+    	print(data)
+    	return redirect('/thankyou.html')
+    else:
+    	return 'this is why I make fun of you'
